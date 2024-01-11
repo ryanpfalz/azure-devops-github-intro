ad5fa5921bed09c3c647dc77ed4064e34cedd9a4
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2024-01-10 18:09:57 -0600

    Add files via upload

--- Devops/Automation/removeChildObjByNestedKey.ps1
+++ Devops/Automation/removeChildObjByNestedKey.ps1
@@ -1,23 +1,28 @@
-# Recursive function to remove a child object by nested key from a JSON object
-function Remove-ConnectionProperties {
+# Recursive function to remove an object by key from a JSON object
+function Remove-KeyFromJson {
     param (
         [PSCustomObject]$JsonObject,
-        [string]$Identifier,
         [string]$KeyToRemove
     )
 
-    foreach ($key in $JsonObject.PSObject.Properties.Name) {
-        $value = $JsonObject.$key
-
-        # Write-Host "Checking key: $key"
-
-        if ($key -eq $Identifier -and $value.PSObject.Properties.Name -contains $KeyToRemove) {
-            # Write-Host "Removing $KeyToRemove from $key"
-            $value.PSObject.Properties.Remove($KeyToRemove)
+    foreach ($prop in $JsonObject.PSObject.Properties) {
+        if ($prop.Value -is [PSCustomObject] -or $prop.Value -is [Collections.Generic.Dictionary[Object, Object]]) {
+            # Recursively call the function for nested objects
+            Remove-KeyFromJson -JsonObject $prop.Value -KeyToRemove $KeyToRemove
         }
-        elseif ($value -is [PSCustomObject] -or $value -is [Collections.Generic.Dictionary[Object, Object]]) {
-            Remove-ConnectionProperties -JsonObject $value -Identifier $Identifier -KeyToRemove $KeyToRemove
+        elseif ($prop.Value -is [Object[]]) {
+            # Handle arrays in the JSON
+            foreach ($item in $prop.Value) {
+                if ($item -is [PSCustomObject] -or $item -is [Collections.Generic.Dictionary[Object, Object]]) {
+                    Remove-KeyFromJson -JsonObject $item -KeyToRemove $KeyToRemove
+                }
+            }
         }
+    }
+
+    # Remove the key if it exists at the current level
+    if ($JsonObject.PSObject.Properties.Name -contains $KeyToRemove) {
+        $JsonObject.PSObject.Properties.Remove($KeyToRemove)
     }
 }
 
@@ -41,14 +46,14 @@ function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
 
 # Example usage:
 
-# In this example, I'm using a sample JSON file that contains an object with an "azureblob_2" key with a child called "connectionProperties" that I want to remove.
-$jsonFile = "<path-to-json>.json"
+# In this example, I'm using a sample JSON file that contains keys called "connectionProperties" that I want to remove.
+$jsonFile = "C:\Users\ryanpfalz\OneDrive - Microsoft\Dev\PowerShell\nestedjson2.json"
 
 # Read the content of the file
 $jsonData = Get-Content $jsonFile -Raw | ConvertFrom-Json
 
 # Process the JSON object
-Remove-ConnectionProperties -JsonObject $jsonData -Identifier "azureblob_2" -KeyToRemove "connectionProperties"
+Remove-KeyFromJson -JsonObject $jsonData -KeyToRemove "connectionProperties"
 
 # Convert the object back to JSON string and format it to remove the unicode characters (from https://stackoverflow.com/a/47779605)
 $updatedJson = @($jsonData) | ConvertTo-Json -Depth 100 | Format-Json | %{
